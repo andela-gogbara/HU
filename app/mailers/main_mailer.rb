5aa7c52e7920f3c56567fe6feee06812f38e7984
Ogbara Godwin <godwin.onisofien@andela.com>
2016-03-15 15:24:47 +0100

    comletes action mailer

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -1,11 +1,13 @@
 class MainMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.main_mailer.notify_question_author.subject
   #
   def notify_question_author(answer)
+
+    sleep 5
+
     @greeting = "Hi"
     @answer = answer
 
---
+++ test/integration/home_page_test.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+feature "Welcome" do
+  
+end
